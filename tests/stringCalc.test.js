8d8e10ca68aa85c8c8218555de533c7b7cc90373
Dharm Joshi <dharmjoshi01@gmail.com>
2025-09-16 13:44:55 +0530

    STEP - 2 Unit-test failed (Return same for single string number)

--- tests/stringCalc.test.js
+++ tests/stringCalc.test.js
@@ -3,3 +3,7 @@ const { addStringAsNumbers } = require('../src/stringCalc');
 test("return 0 for empty string", () => {
   expect(addStringAsNumbers("")).toBe(0);
 });
+
+test("return same number for single number passed", ()=> {
+  expect(addStringAsNumbers("1").toBe(1))
+})
