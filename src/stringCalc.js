1e6a5bcc49deb86a9a29e6b9bc109d59d6b6a240
Dharm Joshi <dharmjoshi01@gmail.com>
2025-09-16 13:55:59 +0530

    Unit test passed ( Return sum of numbers for multiple numbers in string )

--- src/stringCalc.js
+++ src/stringCalc.js
@@ -8,8 +8,17 @@ function addStringAsNumbers(numbers) {
     if(numberArray.length === 1) {
         return Number(numberArray[0])
     }
+
+    let sumOfNumbers = 0;
+
+    for (let number of numberArray) {
+        sumOfNumbers += Number(number)
+    }
+
+    return sumOfNumbers;
 }
 
+addStringAsNumbers("1,3,5")
 module.exports = {
     addStringAsNumbers
 }
