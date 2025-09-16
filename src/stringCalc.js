c9eb0429e4189abf235d1ac14ba5c7a1bdc095ff
Dharm Joshi <dharmjoshi01@gmail.com>
2025-09-16 14:14:47 +0530

    Unit-test passed ( Return sum of number for a custom delimiter )

--- src/stringCalc.js
+++ src/stringCalc.js
@@ -1,23 +1,27 @@
 function addStringAsNumbers(numbers) {
-    if (numbers === "") {
-        return 0;
+  if (numbers === "") {
+    return 0;
+  }
+
+  let numberArray = numbers.split(/[\n,;]/);
+
+  if (numberArray.length === 1) {
+    return Number(numberArray[0]);
+  }
+
+  let sumOfNumbers = 0;
+
+  for (let number of numberArray) {
+    if (isNaN(number)) {
+      continue;
+    } else {
+      sumOfNumbers += Number(number);
     }
+  }
 
-    let numberArray = numbers.split(/[\n,]/);
-
-    if(numberArray.length === 1) {
-        return Number(numberArray[0])
-    }
-
-    let sumOfNumbers = 0;
-
-    for (let number of numberArray) {
-        sumOfNumbers += Number(number)
-    }
-    return sumOfNumbers;
+  return sumOfNumbers;
 }
 
-addStringAsNumbers("1\n2,3")
 module.exports = {
-    addStringAsNumbers
-}
+  addStringAsNumbers,
+};
